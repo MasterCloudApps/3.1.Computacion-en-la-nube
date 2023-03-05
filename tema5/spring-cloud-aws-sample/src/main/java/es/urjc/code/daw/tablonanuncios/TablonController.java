beafbbd8ec3742a579230463b2405813eb1d0482
Maes95 <michel.maes95@gmail.com>
2023-03-05 23:35:09 +0100

    Fix cloud formation examples

--- tema5/spring-cloud-aws-sample/src/main/java/es/urjc/code/daw/tablonanuncios/TablonController.java
+++ tema5/spring-cloud-aws-sample/src/main/java/es/urjc/code/daw/tablonanuncios/TablonController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -43,7 +44,7 @@ public class TablonController {
 		return "tablon";
 	}
 
-	@GetMapping("/anuncio/nuevo")
+	@PostMapping("/anuncio/nuevo")
 	public String nuevoAnuncio(Model model, 
 			@RequestParam String nombre,
 			@RequestParam String asunto,
